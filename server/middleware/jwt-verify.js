766ac000751c60a987ba9b191e9841a3764f69a5
Aaqil yousuf <aaqilyousuf@gmail.com>
2024-05-30 22:14:43 -0400

    added secure logics

--- server/middleware/jwt-verify.js
+++ server/middleware/jwt-verify.js
@@ -3,16 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 export const verifyToken = async (req, res, next) => {
-  const token = req.header["authorization"];
-  console.log(token);
-  if (!token) {
+  const authHeader = req.headers["authorization"];
+  console.log(authHeader);
+  // console.log("secret key: ", process.env.JWT_SECRET_KEY);
+  // console.log("Request Headers:", req.headers);
+  // const authHeader = req.headers["authorization"];
+  // console.log("Authorization Header:", authHeader);
+  if (!authHeader) {
     return res.status(401).json({ msg: "unauthorized ithula tha" });
   }
+  let token;
+  if (authHeader.startsWith("Bearer")) {
+    token = authHeader.split("")[1];
+  } else {
+    token = authHeader;
+  }
+  console.log("extracted token: ", token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.userId = decoded.userId;
     next();
   } catch (err) {
+    console.error("here is error: ", err);
     return res.status(401).json({ msg: "unauthorized" });
   }
 };
