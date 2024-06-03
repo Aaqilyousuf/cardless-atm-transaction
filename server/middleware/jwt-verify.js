8b0ca1192c6e7618d216815760d5ab874b9a9057
Aaqil yousuf <aaqilyousuf@gmail.com>
2024-06-03 06:42:33 -0400

    changes in authController

--- server/middleware/jwt-verify.js
+++ server/middleware/jwt-verify.js
@@ -4,27 +4,25 @@ import dotenv from "dotenv";
 dotenv.config();
 export const verifyToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  console.log(authHeader);
-  // console.log("secret key: ", process.env.JWT_SECRET_KEY);
-  // console.log("Request Headers:", req.headers);
-  // const authHeader = req.headers["authorization"];
   // console.log("Authorization Header:", authHeader);
+
   if (!authHeader) {
-    return res.status(401).json({ msg: "unauthorized ithula tha" });
+    return res.status(401).json({ msg: "Unauthorized: No token provided" });
   }
-  let token;
-  if (authHeader.startsWith("Bearer")) {
-    token = authHeader.split("")[1];
-  } else {
-    token = authHeader;
-  }
-  console.log("extracted token: ", token);
+
+  const token = authHeader.startsWith("Bearer ")
+    ? authHeader.split(" ")[1]
+    : authHeader;
+  // console.log("Extracted Token:", token);
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log("Decoded token: " + decoded);
+    console.log("Decoded token userId: " + decoded.userId);
     req.userId = decoded.userId;
     next();
   } catch (err) {
-    console.error("here is error: ", err);
-    return res.status(401).json({ msg: "unauthorized" });
+    console.error("Token verification error:", err);
+    return res.status(401).json({ msg: "Unauthorized: Invalid token" });
   }
 };
